a85e0dbec2fef629b22511f89bcb8bfb8070f613
Victor Cuellar <vcuell13@stu.southtexascollege.edu>
2026-09-13 22:28:46 -0500

    Added Section

---
+++ src/components/Section.vue
@@ -0,0 +1,27 @@
+<script setup>
+const props = defineProps({
+  title: {
+    type: String,
+    default: 'Section'
+  }
+})
+</script>
+
+<template>
+  <section>
+            <h3>{{title}}</h3>
+            <slot></slot>
+        </section>
+</template>
+
+<style scooped>
+section {
+    margin-top: 16px;
+}
+
+Section h3{
+    margin: 0 0 6px;
+    font-size: 14px;
+    color: #ca0a0a;
+}
+</style>
